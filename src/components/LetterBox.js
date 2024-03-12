4e877b8a7e38bffaab6bfdb8cfec506b710100ae
Deniz <denizandac33@gmail.com>
2024-03-12 08:22:56 +0300

    Update LetterBox
    + added background color

--- src/components/LetterBox.js
+++ src/components/LetterBox.js
@@ -1,6 +1,9 @@
-export default function LetterBox({ value }) {
+export default function LetterBox({ value, isIncluded }) {
   return (
-    <div className="letter-box">
+    <div
+      className="letter-box"
+      style={{ backgroundColor: isIncluded ? "lightgreen" : "" }}
+    >
       <input type="text" maxLength="1" disabled value={value} />
     </div>
   );
