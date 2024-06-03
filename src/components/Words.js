1af615e26c057cbb14c293fe6eb13410325ab2e9
Deniz <denizandac33@gmail.com>
2024-06-04 02:50:40 +0300

    Word Checker
    +added helper function to check whether  the word is eligible or not

--- src/components/Words.js
+++ src/components/Words.js
@@ -1,22 +1,35 @@
 import { useState } from "react";
 import ExpectedWord from "./ExpectedWord";
+import { EligibilityChecker, LetterChecker } from "../utils/WordChecker";
+
+//const dictionary = ["HELLO", "WORLD", "REACT"];
+//import from english dictionary API
 
 export default function Words({ guessCount, letterCount }) {
   const [word, setWord] = useState("");
   const [guessIndex, setGuessIndex] = useState(0);
-
-  const wordLength = word.length;
-  const guessedWords = Array(guessCount).fill("");
+  const [guessedWords, setGuessedWords] = useState(Array(guessCount).fill(""));
 
   const handleKeyChange = ({ key }) => {
     if (key === "Backspace") {
       setWord((prevWord) => prevWord.slice(0, -1));
     } else if (key === "Enter") {
-      setGuessIndex((prevGuessIndex) =>
-        prevGuessIndex < guessCount - 1 ? prevGuessIndex + 1 : prevGuessIndex
+      if (!EligibilityChecker(word, letterCount, "dictionary")) {
+        //shake the word
+        return;
+      } else if (guessIndex === guessCount) {
+        return;
+        //game over modal shows up
+      }
+      const calculatedWord = LetterChecker(word, "BEYZA");
+      setGuessedWords((prevGuessedWords) =>
+        prevGuessedWords.map((prevGuessedWord, index) =>
+          index === guessIndex ? calculatedWord : prevGuessedWord
+        )
       );
+      setGuessIndex((prevGuessIndex) => prevGuessIndex + 1);
       setWord("");
-    } else if (wordLength === letterCount) {
+    } else if (word.length === letterCount) {
       return;
     } else if (key.length === 1) {
       if (key.match(/[a-z]/i)) {
@@ -32,10 +45,11 @@ export default function Words({ guessCount, letterCount }) {
         <ExpectedWord
           key={index}
           letterCount={letterCount}
-          enteredValue={index === guessIndex ? word : undefined}
+          enteredValue={index === guessIndex ? word : guessedWords[index]}
+          mode={index === guessIndex ? "active" : "inactive"}
         />
       ))}
-      {wordLength},{letterCount},{word}, {guessIndex}, {guessCount}
+      {word}
     </div>
   );
 }
--- src/utils/wordChecker.js
+++ src/utils/wordChecker.js
@@ -1,8 +1,25 @@
-export function lessLength(word, dictionary) {
-  if (word.length < 5) {
-    return false;
-  } else if (!dictionary.includes(word)) {
+export function EligibilityChecker(word, letterCount, dictionary) {
+  if (word.length < letterCount) {
     return false;
   }
+  //  else if (!dictionary.includes(word)) {
+  //   return false;
+  // }
   return true;
+}
+export function LetterChecker(word, expectedWord) {
+  let letterCount = 0;
+  let newWord = [];
+  for (let i = 0; i < word.length; i++) {
+    newWord.push({ letter: word[i], type: "none" });
+    for (let j = 0; j < expectedWord.length; j++) {
+      if (word[i] === expectedWord[j]) {
+        if (i === j) newWord[i].type = "exactPosition";
+        else newWord[i].type = "includes";
+        letterCount++;
+        break;
+      }
+    }
+  }
+  return newWord;
 }
