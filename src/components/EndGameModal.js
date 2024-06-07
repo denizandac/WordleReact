2983010bed86875f51ac5cd3a934a1d9b3ddf38d
Deniz <denizandac33@gmail.com>
2024-06-07 13:19:22 +0300

    Create End Game Modal Component

---
+++ src/components/EndGameModal.js
@@ -0,0 +1,10 @@
+export function EndGameModal() {
+  return (
+    <modal className="end-game-modal">
+      <h1>Game Over</h1>
+      <p>Would you like to play again?</p>
+      <button>Yes</button>
+      <button>No</button>
+    </modal>
+  );
+}
