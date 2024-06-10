071a1eace592d4bd2aefff3823850790707566d4
Deniz <denizandac33@gmail.com>
2024-06-10 16:20:22 +0300

    Connected to Context
    +addd ref property to connect modal to context

--- src/components/EndGameModal.js
+++ src/components/EndGameModal.js
@@ -1,10 +1,16 @@
+import { useRef, useContext } from "react";
+import { WordContext } from "../store/word-context";
+
 export function EndGameModal() {
+  const dialogRef = useRef();
+  const wordCtx = useContext(WordContext);
+  wordCtx.setModalRef(dialogRef.current);
   return (
-    <modal className="end-game-modal">
+    <dialog ref={dialogRef} className="end-game-modal">
       <h1>Game Over</h1>
       <p>Would you like to play again?</p>
       <button>Yes</button>
       <button>No</button>
-    </modal>
+    </dialog>
   );
 }
