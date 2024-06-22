6bcf4e356cde325f00b965266ea15c8cc0c6a98c
Deniz <denizandac33@gmail.com>
2024-06-22 22:50:41 +0300

    End Game Modal Update
    +added .css to modify the style

--- src/components/EndGameModal.js
+++ src/components/EndGameModal.js
@@ -1,16 +1,19 @@
 import { useRef, useContext } from "react";
 import { WordContext } from "../store/word-context";
+import "./EndGameModal.css";
 
 export function EndGameModal() {
   const dialogRef = useRef();
   const wordCtx = useContext(WordContext);
   wordCtx.setModalRef(dialogRef.current);
+
   return (
     <dialog ref={dialogRef} className="end-game-modal">
-      <h1>Game Over</h1>
-      <p>Would you like to play again?</p>
-      <button>Yes</button>
-      <button>No</button>
+      <h1>Would you like to play again?</h1>
+      <div className="buttons">
+        <button>Yes</button>
+        <button>No</button>
+      </div>
     </dialog>
   );
 }
